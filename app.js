2b01a3efb792dc8ea9b8276ce88ce3b95ee7416a
Thiago Souza <thiagosouza1421@hotmail.com>
2023-01-06 21:33:29 -0300

    Adicionando outras funções.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let buracoNegro = new BuracoNegro(10, 0, 0);
+let buracoNegro = new BuracoNegro(10, 10, 15);
 let acceleration = buracoNegro.aceleracaoGravitacional(1, 1);
 
 console.log(`Aceleração gravitacional: ${acceleration} m/s ao quadrado`);
--- buracoNegro.js
+++ buracoNegro.js
@@ -2,12 +2,19 @@
 Introdução à Astronomia
 Leis de Newton e
 Forças Gravitacionais - Rogério Riffel
-
 */
 
 const CONSTATE_GRAVITACIONAL = 6.674e-11;
 
 const BuracoNegro = function(x, y , massa){
+    /* Criterios:
+        Massa precisa ser maior que 0;
+        Coordenadas precisam existir
+    */
+
+    this.x = x;
+    this.y = y;
+
     this.massa = massa;
     //Retorna o calculo da força gravicational exercida pelo buraco negro em um objeto
     this.forcaGravitacional = (massaObjeto, distancia)=>{
@@ -18,5 +25,15 @@ const BuracoNegro = function(x, y , massa){
     this.aceleracaoGravitacional = (massaObjeto, distancia) => {
         return this.forcaGravitacional(massaObjeto, distancia) / massaObjeto;
       }
+
+    //Retorna o valor da força gravitacional exercida pelo buraco negro em objetos com diferentes posições
+    this.calcularForcaDe = (x, y) =>{
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+    }
+
+    //Retorna a velocidade de escape do buraco negro
+    this.velocidadeDeEscape = () =>{
+        return Math.sqrt((CONSTATE_GRAVITACIONAL * 2 * this.massa) / this.calcularForcaDe(0, 0));
+    }
 
 }
